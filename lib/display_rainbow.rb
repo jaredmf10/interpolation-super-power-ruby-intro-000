e9d78afd48b80046206f3824a134f306ceb5d635
Jared Fleming <jaredmf10@gmail.com>
2020-01-18 15:21:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -9,7 +9,7 @@ def display_rainbow(colors)
   #puts "The fifth color of the rainbow is #{colors[4]}"
   #puts "The sixth color of the rainbow is #{colors[5]}"
   #puts "The seventh color of the rainbow is #{colors[6]}"
-  #puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+  puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
   #puts "#{colors[0]}, #{colors[1]}, #{colors[2]}, #{colors[3]}, #{colors[4]}, #{colors[5]}, #{colors[6]}"
 
 end
